ca6d01be281710eb083e84d3b77d16dd23cb77c8
karorocad <168958300+karorocad@users.noreply.github.com>
2025-01-19 16:55:30 +0200

    Add files via upload

---
+++ DK_httpInfo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper;
+using Grasshopper.Kernel;
+
+namespace DK_http
+{
+    public class DK_httpInfo : GH_AssemblyInfo
+    {
+        public override string Name => "DK_Http";
+
+        //Return a 24x24 pixel bitmap to represent this GHA library.
+        public override Bitmap Icon => null;
+
+        //Return a short string describing the purpose of this GHA library.
+        public override string Description => "Rebuild of Bengesht Http Server for use in Rhino 8";
+
+        public override Guid Id => new Guid("8adde728-bc48-45f6-80d4-abb37a8834ae");
+
+        //Return a string identifying you or your company.
+        public override string AuthorName => "David Kay";
+
+        //Return a string representing your preferred contact details.
+        public override string AuthorContact => "karorocad@gmail.com";
+
+        //Return a string representing the version.  This returns the same version as the assembly.
+        public override string AssemblyVersion => GetType().Assembly.GetName().Version.ToString();
+
+        public static string getComponentDescription(string str)
+        {
+            string output = str +
+                "DK Http Server for GH";
+
+            return output;
+        }
+    }
+}
---
+++ NetworkComponent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace DK_http.HttpCat
+{
+abstract public class HttpComponent : GH_Component
+{
+    public HttpComponent(string name, string nickname, string description)
+        : base(name, nickname, description, "DK Http Server", "HTTP")
+    {
+    }
+}//eoc
+}//eons
